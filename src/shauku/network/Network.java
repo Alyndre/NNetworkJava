586aec26793b45e323bbfc10854c977e5e46e26b
Daniel Gomez <dgomez@ilernaonline.com>
2016-05-24 19:16:13 +0200

    major changes

--- src/shauku/network/Network.java
+++ src/shauku/network/Network.java
@@ -6,27 +6,31 @@ public class Network {
     private ArrayList<Neuron> inList;
     private ArrayList<Neuron> outList;
     private ArrayList<ArrayList<Neuron>> hidLayers;
+    private float learnRate;
 
-    public Network(Long inputs, Long outputs, Long[] hidden){
+    public Network(long inputs, long outputs, long[] hidden, float learnRate){
         System.out.println("Assembling network...");
 
         hidLayers = new ArrayList<>();
         inList = new ArrayList<>();
         outList = new ArrayList<>();
 
+        System.out.println("Learning rate of: " + this.learnRate);
+        this.learnRate = learnRate;
+
         System.out.println("Creating input layer...");
         for (Long i = 0L; i < inputs; i++){
             Neuron n = new Neuron(0f);
             inList.add(n);
         }
-        System.out.println("Done!");
+        System.out.println("Input layer created!");
 
         System.out.println("Creating hidden layers...");
         System.out.println("Number of hidden layers: " + hidden.length);
         for (int x = 0; x<hidden.length; x++){
-            Long l = hidden[x];
+            long l = hidden[x];
             ArrayList<Neuron> layer = new ArrayList<>();
-            for (Long i = 0L; i<l; i++){
+            for (long i = 0L; i<l; i++){
                 Neuron n = new Neuron(0f);
                 if (x==0){
                     n.connect(inList);
@@ -37,20 +41,20 @@ public class Network {
             }
             hidLayers.add(layer);
         }
-        System.out.println("Done!");
+        System.out.println("Hidden layers created!");
 
         System.out.println("Creating output layer...");
-        for (Long i = 0L; i < outputs; i++){
+        for (int i = 0; i < outputs; i++){
             Neuron n = new Neuron(0f);
             n.connect(hidLayers.get(hidLayers.size()));
             outList.add(n);
         }
-        System.out.println("Done!");
+        System.out.println("Output layer created!");
 
         System.out.println("Network online!");
     }
 
-    public void feed(Double[] data) {
+    public void feed(double[] data) {
         if (data.length == inList.size()){
             for (int x = 0; x<data.length; x++) {
                 Neuron n = inList.get(x);
@@ -61,11 +65,19 @@ public class Network {
         }
     }
 
-    public void train(){
+    public void train(double[][] data, double[] expected){
 
     }
 
     public void start() {
+        outList.stream().map(Neuron::fire).forEach(v -> System.out.println("Neuron output: " + v));
+    }
+
+    public void saveNet() {
+
+    }
+
+    public void loadNet(long netId) {
 
     }
 
--- src/shauku/neuronal/Main.java
+++ src/shauku/neuronal/Main.java
@@ -1,47 +1,30 @@
 package shauku.neuronal;
 
-//import java.lang.Math;
-
 import shauku.network.Network;
-import shauku.network.Neuron;
 
 public class Main {
 
+    //http://www.nnwj.de/forwardpropagation.html
+    //http://www.nnwj.de/backpropagation.html
+
     public static void main(String[] args) {
-        Long inputs = 2L;
-        Long outputs = 1L;
-        Long[] hidden = {4L};
+        long inputs = 2L;
+        long outputs = 1L;
+        long[] hidden = {4L};
 
-        Float learnRate = 0.25f;
+        float learnRate = 0.25f;
 
-        Boolean[][] data = {
-                {false, false},
-                {false, true},
-                {true, false},
-                {true, true}
+        double[][] data = {
+                {0, 0},
+                {0, 1},
+                {1, 0},
+                {1, 1}
         };
-        Network net = new Network(inputs, outputs, hidden);
+
+        double[] expected = {0, 1, 1, 0};
+
+        Network net = new Network(inputs, outputs, hidden, learnRate);
+        net.train(data, expected);
         net.start();
-    }
-
-    public static void train(Boolean[][] inputs, Neuron a, Neuron b, Neuron c, Neuron d, Neuron xor) {
-        //http://www.nnwj.de/forwardpropagation.html
-        //http://www.nnwj.de/backpropagation.html
-        int iteration = 0;
-        for (Boolean[] values : inputs) {
-            for (Boolean val : values) {
-                //Input Layer on the fly
-                Neuron inputN = new Neuron(0.0f);
-                inputN.setWeight(val);
-                a.connect(inputN);
-                b.connect(inputN);
-                c.connect(inputN);
-                d.connect(inputN);
-            }
-
-            xor.fire();
-            System.out.println(iteration + " - Values: " + values[0] + "," + values[1] + " - Result: " + xor.isFired());
-            iteration++;
-        }
     }
 }
