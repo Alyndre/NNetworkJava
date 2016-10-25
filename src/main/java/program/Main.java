85939277a7b0290e1ec5a3b781e5419d86c825a3
Shauku <n3rv.apps@gmail.com>
2016-10-26 00:28:57 +0200

    spark'd

--- src/main/java/program/Main.java
+++ src/main/java/program/Main.java
@@ -4,6 +4,7 @@ import data.Data;
 import data.XORData;
 import network.MLP.MultiLayerPerceptron;
 import network.Network;
+import static spark.Spark.*;
 
 public class Main {
 
@@ -11,22 +12,30 @@ public class Main {
     //http://www.nnwj.de/backpropagation.html
 
     public static void main(String[] args) {
-        long inputs = 2L;
-        long outputs = 1L;
-        long[] hidden = {4L};
 
-        float learnRate = 0.25f;
+        get("/", (request, response) -> {
 
-        Data dataset = new XORData();
+            long inputs = 2L;
+            long outputs = 1L;
+            long[] hidden = {4L};
 
-        Network net = new MultiLayerPerceptron(inputs, outputs, hidden, learnRate);
-        net.train(dataset, 10000);
+            float learnRate = 0.25f;
 
-        for (double[] d : dataset.getData()){
-            System.out.println(d[0]+","+d[1]);
-            net.feed(d);
-            net.start();
-        }
+            Data dataset = new XORData();
+
+            Network net = new MultiLayerPerceptron(inputs, outputs, hidden, learnRate);
+            net.train(dataset, 10000);
+
+            for (double[] d : dataset.getData()){
+                System.out.println(d[0]+","+d[1]);
+                net.feed(d);
+                net.start();
+            }
+
+            return "Ok";
+        });
+
+
     }
 }
 
